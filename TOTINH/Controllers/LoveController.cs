34a2170b0727026733e5fcf5597ce66198e334a4
huytr <huytr@DESKTOP-63PGR1N>
2022-04-26 10:06:05 +0700

    Add project files.

---
+++ TOTINH/Controllers/LoveController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TOTINH.Models;
+
+namespace TOTINH.Controllers
+{
+    public class LoveController : Controller
+    {
+        // GET: Love
+        [HttpGet]
+        public ActionResult GHILOI()
+        {
+            ViewBag.Check = false;
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //Controller lấy tin nhắn gửi về Model
+        public ActionResult GHILOI(LoiNhan value_message)
+        {
+            //sau khi submit sẽ kiểm tra đã nhập tên và tin nhắn chưa
+           if(!String.IsNullOrEmpty(value_message.yourname) && !String.IsNullOrEmpty(value_message.message))
+            {
+                //khai báo đối tượng send để đẩy tin nhắn và tên về Model
+                var send = new LoiNhan();
+                send.add_message(value_message.yourname, value_message.message);
+                
+                
+                ModelState.Clear();
+                ViewBag.Check = true;
+                ViewBag.Message = "Link";
+                ViewBag.Controller = Url.Action("NHANLOINHAN", "Love", new { id = send.id_message }, this.Request.Url.Scheme);
+                return View();
+            }else ViewBag.Check = false;
+            return View(value_message);
+        }
+        [HttpGet]
+        //Controller kiểm tra id tin nhắn đã có chưa nếu chưa thì quay về phần gửi tin nhắn
+        public ActionResult NHANLOINHAN(int id)
+        {
+            ViewBag.Check = false;
+            if(id == null)
+            {
+                return RedirectToAction("GHILOI", "Love");
+            }
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        //Controller đưa tin nhắn lên View nếu HttpGet có được id
+        public ActionResult NHANLOINHAN(int id, LoiNhan reciever)
+        {
+            
+          
+            // nếu mật khẩu trùng với mật khẩu của tin nhắn 
+            if(reciever.password == reciever.get_password(id))
+            {
+                //đẩy tin nhắn lên View
+                string message_reciever = reciever.get_message(id);
+                ViewBag.Message = message_reciever;
+                ViewBag.name = reciever.get_password(id);
+                ViewBag.Check = true;
+                return View();
+                
+            }
+            else
+            {
+                //nếu nhập sai mật khẩu Controller đẩy thông báo lên View
+                ViewBag.Check = false;
+                ModelState.AddModelError(String.Empty, "mật khẩu bị sai, vui lòng nhập lại");
+            }
+            
+            return View(reciever);
+        }
+
+    }
+}
---
+++ TOTINH/Models/LoiNhan.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TOTINH.Models
+{
+    public class LoiNhan
+    {
+        
+        [Required(AllowEmptyStrings = false,ErrorMessage = "không được bỏ trống")]
+        [StringLength(255, ErrorMessage = "Không được nhập quá 255 kí tự", MinimumLength = 1)]
+        public string yourname { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "không được bỏ trống")]
+        [StringLength(255, ErrorMessage = "Không được nhập quá 255 kí tự",MinimumLength = 1)]
+        //[RegularExpression(@"[_-,/`~().\r]",ErrorMessage = "Không có các kí tự đặc biệt")]
+        
+        //khai báo biến cho tên và tin nhắn
+        public string message { get; set; }
+
+        public int id_message;
+
+        //khai báo mật khẩu
+        [Required(AllowEmptyStrings = false, ErrorMessage = "không được bỏ trống")]
+        [StringLength(255, ErrorMessage = "Không được nhập quá 255 kí tự", MinimumLength = 1)]
+        public string password { get; set; }
+
+        //khai báo chuỗi kết nối sql
+
+        private string ConSqlStr = @"Data Source=DESKTOP-63PGR1N\SQLEXPRESS;Initial Catalog=NHANGUIYEUTHUONG;Integrated Security=True";
+
+        //hàm thêm tin nhắn vào data
+        public void add_message (string name, string message)
+        {
+
+            //tạo đối tượng random
+            Random random = new Random();            
+            int ma = 1;
+            //kiểm tra mã vừa random
+            using (SqlConnection sql = new SqlConnection(ConSqlStr))
+            {
+                sql.Open();
+                while (true)
+                {
+
+                    
+                    //tạo mã
+                    ma = random.Next(1, 1000);
+                    //truy vấn để kiểm tra mã
+                    SqlCommand sqlcmd1 = new SqlCommand($"select * from LOINHAN where id_message = {ma}", sql);
+                    var reader = sqlcmd1.ExecuteReader();
+                    //nếu ko có thì dừng lại
+                    if (!reader.HasRows)
+                    {
+                        reader.Close();
+                        
+                        break;
+                    }
+                    else
+                    {
+                        reader.Close();
+                        
+                    }
+                }
+                //thêm mã
+                SqlCommand sqlcmd = new SqlCommand($"INSERT INTO LOINHAN Values (@name,@message,'{DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss")}',{ma})", sql);
+                id_message = ma;
+                sqlcmd.Parameters.Add("name",SqlDbType.NVarChar).Value = name;
+                sqlcmd.Parameters.Add("message", SqlDbType.NVarChar).Value = message;
+                sqlcmd.ExecuteNonQuery();
+                sql.Close();
+            }
+               
+        }
+
+        //hàm lấy tin nhắn
+        public string get_message(int id)
+        {
+            //mở kết nối sql
+            using(SqlConnection sql = new SqlConnection(ConSqlStr))
+            {
+                sql.Open();
+                SqlCommand sqlcmd = new SqlCommand($"select message from LOINHAN where id_message = {id}", sql);
+                string reader = sqlcmd.ExecuteScalar().ToString();
+                return reader;
+            }
+        }
+
+        //hàm lấy tên
+        public string get_password(int id)
+        {
+            using (SqlConnection sql = new SqlConnection(ConSqlStr))
+            {
+                sql.Open();
+                SqlCommand sqlcmd = new SqlCommand($"select name from LOINHAN where id_message = {id}", sql);
+                string reader = sqlcmd.ExecuteScalar().ToString();
+                return reader;
+            }
+        }
+
+
+        
+    }
+}
